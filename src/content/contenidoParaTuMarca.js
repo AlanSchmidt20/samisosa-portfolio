d51ce8b48ad38ee50023c485f1928f10d66739ba
Alan Schmidt <alu.schmidt@hotmail.com>
2023-04-13 21:50:52 -0400

    Update contenidoParaTuMarca.js

--- src/content/contenidoParaTuMarca.js
+++ src/content/contenidoParaTuMarca.js
@@ -1,45 +1,45 @@
 export const leftImages = [
   {
-    id: "1",
+    id: 1,
     image: require("../images/contenidoParaTuMarca-image1.JPG"),
   },
   {
-    id: "2",
+    id: 2,
     image: require("../images/contenidoParaTuMarca-image2.PNG"),
   },
   {
-    id: "3",
+    id: 3,
     image: require("../images/contenidoParaTuMarca-image3.JPG"),
   },
 ];
 
 export const items = [
   {
-    id: "1",
+    id: 1,
     description: "PACKS DE CONTENIDO PARA TU MARCA",
     link:
       "https://wa.me/5491161705336?text=Hola+Sami%21+Me+interesa+tener+m%C3%A1s+informaci%C3%B3n+sobre+como+trabajas+y+el+presupuesto+de+los+distintos+Packs+de+contenido+que+ofreces%2C+gracias%21+%F0%9F%A4%8D",
   },
   {
-    id: "2",
+    id: 2,
     description: "SERVICIOS COMO MODELO",
     link:
       "https://wa.me/5491161705336?text=Hola+Sami%21+Me+encantar%C3%ADa+tener+m%C3%A1s+informaci%C3%B3n+sobre+tus+servicios+como+modelo%2C+gracias%F0%9F%A4%8D",
   },
   {
-    id: "3",
+    id: 3,
     description: "SERVICIOS DE CANJE",
     link:
       "https://wa.me/5491161705336?text=Hola+Sami%21+Me+gustaria+obtener+m%C3%A1s+informaci%C3%B3n+sobre+tus+servicios+de+canje%2C+gracias%21+%F0%9F%A4%8D",
   },
   {
-    id: "4",
+    id: 4,
     description: "SERVICIOS DE EDICIÓN",
     link:
       "https://wa.me/5491161705336?text=Hola+Sami%21+Me+gustaria+obtener+m%C3%A1s+informaci%C3%B3n+sobre+tus+servicios+de+edici%C3%B3n%2C+gracias%21+%F0%9F%A4%8D",
   },
   {
-    id: "5",
+    id: 5,
     description: "SERVICIOS DE PRESENCIA + CONTENIDO",
     link:
       "https://wa.me/5491161705336?text=Hola+Sami%21+Me+gustaria+obtener+m%C3%A1s+informaci%C3%B3n+sobre+tus+servicios+de+presencia+y+contenido%2C+gracias%21+%F0%9F%A4%8D",
@@ -48,15 +48,15 @@ export const items = [
 
 export const rightImages = [
   {
-    id: "1",
+    id: 1,
     image: require("../images/contenidoParaTuMarca-image4.JPG"),
   },
   {
-    id: "2",
+    id: 2,
     image: require("../images/contenidoParaTuMarca-image5.JPG"),
   },
   {
-    id: "3",
+    id: 3,
     image: require("../images/contenidoParaTuMarca-image6.JPG"),
   },
 ];
